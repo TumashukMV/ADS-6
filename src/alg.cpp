609b366450cb0bbfee31beba4087e37463f68976
TumashukMV <65454334+TumashukMV@users.noreply.github.com>
2020-05-31 19:58:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,44 +3,39 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include  <string.h>
-#include  <ctype.h>
-
 BST<std::string> makeTree(char* filename)
 {
-using namespace std;
-
-BST<string> makeTree(char* filename)
-
-} 
-{
-
-BST<string> tree1;
-    ifstream file(filename);
-    string str="", tmp="";
-    while (file >> str)
-{
-    int flag = 0;
-    for (int j = 0; j < str.size(); j++)
-    {
-        flag = 0;
-        if ((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= 'A' && str[j] <= 'Z'))
-            tmp += (char)tolower(str[j]);
-        else
-            if (tmp != "")
-            {
-                tree1.add(tmp);
-                tmp = "";
-                flag = 1;
-                continue;
-            }
-    }
-    if (flag == 0 && tmp != "")
-    {
-        tree1.add(tmp);
-        tmp = "";
-    }
+std::string word;
+	std::ifstream file(filename);
+	BST<std::string>* tree = new BST<std::string>;
+	char str = ' ';
+	while (!file.eof()) 
+   {
+		while (str < 'A' && (!file.eof()))
+	  {
+			file.get(str);
+		}
+		while (str >= 'A' && (!file.eof()))
+		{
+			if (str >= 'A' && str <= 'Z')
+			{
+				word += str;
+			}
+			if (str >= 'a' && str <= 'z')
+			{
+				word += str;
+			}
+			file.get(str);
+		}
+		for (int i = 0; i < word.length(); i++)
+		{
+			if (word[i] >= 'A' && word[i] <= 'Z')
+				word[i] += 32;
+		}
+		(*tree).add(word);
+		word = "";
+	}
+	return *tree;
 }
-    return tree1;
-
 }
+
