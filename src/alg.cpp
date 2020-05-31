99c1523ef865c58877ebea2b25d62c7345d0a012
TumashukMV <65454334+TumashukMV@users.noreply.github.com>
2020-05-31 19:16:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,36 +1,41 @@
+#include  "bst.h"
+#include  <iostream>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-using namespace std;
 
 BST<std::string> makeTree(char* filename)
 {
+  std::string word;
+	std::ifstream file(filename);
+	BST<std::string>* tree = new BST<std::string>;
+	char tempStr = ' ';
+	while (!file.eof()) {
 
 } 
-BST<string> tree;
-    ifstream file(filename);
-    string str,word;
-    while (!file.eof()) {
-        word.clear();
-        file >> str;
-        bool flag = 0;
-        for (auto &letter : str) {
-            if (isalpha(letter))
-            {
-                flag = 1;
-                letter=tolower(letter);
-                word += letter;
-            }
-            else if (flag)
-            {
-                tree.add(word);
-                word.clear();
-                flag = 0;
-            }
-        }
-        if (!word.empty()) {
-            tree.add(word);
-        }
-    }
-    return tree;
+		while (tempStr < 65 && (!file.eof())) {
+			file.get(tempStr);
+		}
+
+		while (tempStr >= 65 && (!file.eof())) {
+			if (tempStr >= 65 && tempStr <= 90) {
+				word += tempStr;
+			}
+			if (tempStr >= 97 && tempStr <= 122) {
+				word += tempStr;
+			}
+
+			file.get(tempStr);
+		}
+
+		for (int i = 0; i < word.length(); i++) {
+			if (word[i] >= 65 && word[i] <= 90)
+				word[i] += 32;
+		}
+
+		(*tree).Add(word);
+		word = "";
+	}
+
+	return *tree;
 }
