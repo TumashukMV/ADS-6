b23d97ee5e7652852e8dae1da1ec45250811a7d0
TumashukMV <65454334+TumashukMV@users.noreply.github.com>
2020-05-31 19:35:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,30 +5,30 @@
 #include  <cstdlib>
 
 BST<std::string> makeTree(char* filename)
+{std::string word;
 {
 std::string word;
 	std::ifstream F(filename);
 	BST<std::string> *tree = new BST<std::string>;
 	char str = ' ';
 	while (!F.eof()) {
-
-} 
 		while ( str < 65 && (!F.eof()) ) 
 		{
 			F.get(str);
 		}
+(  str >= 65  && (!F.eof()))
+
+
 while (  str >= 65  && (!F.eof()))
 {
 	if (str >= 65 && str <= 90)
 	{
-
 		word += str;	
 	}
 	if (str >= 97 && str <= 122)
 	{
 		word += str;
 	}
-
 	F.get(str);	
 }
 for (int i = 0; i < word.length(); i++)
@@ -36,9 +36,11 @@ for (int i = 0; i < word.length(); i++)
 	if (word[i] >= 65 && word[i] <= 90)
 		word[i] += 32;
 }
-
 (*tree).add(word);
 word = "";
 	}
-	return *tree;    
+
+
+	return *tree;
+
 }
